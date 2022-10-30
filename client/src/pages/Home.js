4b5a524da3898792c84a5b00438ee4d794b4010c
Mayfield Mayfield <mayfieldmel@gmail.com>
2022-10-30 17:54:06 -0400

    trouble shooting build

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,1 +1,13 @@
 import React from 'react';
+import logo from "../images/player-up-logo.png"
+
+const Home = () => {
+
+    return (
+       <main>
+        <div>
+            <img src={logo}></img>
+        </div>
+       </main>
+    );
+} 
