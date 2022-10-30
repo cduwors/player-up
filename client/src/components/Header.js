7060776aa3c506d09225a2a7b13d5b5c2ddfab23
Mayfield Mayfield <mayfieldmel@gmail.com>
2022-10-30 19:11:20 -0400

    trouble-shooting page display

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,38 +1,14 @@
 import React from 'react';
-import { Link } from "react-router-dom";
-// import Auth from "../../utils/auth";
-import logo from "../images/player-up-logo.png"
+
+// import logo from "../images/player-up-logo.png"
 
 const Header = () => {
-  // logout
-  const logout = (event) => {
-    event.preventDefault();
-    // Auth.logout();
-  };
-
   return (
     <header className="header">
-      <div className="">
-        <Link to="/">
-          <span><img src={logo} alt="player-up-logo"></img></span>
-        </Link>
-
-        <nav className="text-center">
-          {/* {Auth.loggedIn() ? ( */}
-            <>
-              <Link to="/profile">Me</Link>
-              <a href="/" onClick={logout}>
-                Logout
-              </a>
-            </>
-          {/* ) : (
-            <>
-              <Link to="/login">Login</Link>
-              <Link to="/signup">Signup</Link>
-            </>
-          )} */}
-        </nav>
-      </div>
+      <nav>
+        <div>Events</div>
+        <div>Login/Signup</div>
+      </nav>
     </header>
   );
 };
