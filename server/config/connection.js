b8e96d0f3d38605765b8cb17a87fb1c626ecc509
Mayfield Mayfield <mayfieldmel@gmail.com>
2022-10-30 17:39:05 -0400

    up to date

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,20 +1,10 @@
-<<<<<<< HEAD
-const mongoose = require("mongoose");
-
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks", {
-=======
 const mongoose = require('mongoose');
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/playerup', {
->>>>>>> 31bf182f7492e230839c0421f9f9c3877d4c64a1
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
   useFindAndModify: false,
 });
 
-<<<<<<< HEAD
 module.exports = mongoose.connection;
-=======
-module.exports = mongoose.connection;
->>>>>>> 31bf182f7492e230839c0421f9f9c3877d4c64a1
