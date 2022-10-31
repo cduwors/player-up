ba43d1a6d84369c909022aa903cf7f8213de0124
Mayfield Mayfield <mayfieldmel@gmail.com>
2022-10-31 14:23:06 -0400

    clean repo

--- client/src/components1/SignupForm.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const SignupForm = () => {
-  return (
-    <div>Singup Form</div>
-  );
-};
-
-export default SignupForm;
--- client/src/pages1/SingleEvent.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const SingleEvent = () => {
-  return (
-    <div>SingleEvent</div>
-  );
-};
-
-export default SingleEvent;
