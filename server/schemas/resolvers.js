3413bbfae20140191cf738c1c29c983c8496b1b9
Morgan Gilbert <morgan.el.gilbert@gmail.com>
2022-11-01 18:51:34 -0400

    added resolver mutation code, updated with code from develop

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,81 +1,43 @@
-<<<<<<< HEAD
-// Define the query and mutation functionality to work with the Mongoose models
-
-const { AuthenticationError } = require('apollo-server-express');
-const { User, Events } = require('../models');
-const { signToken } = require('../utils/auth');
-
-// TODO User type: _id, username, email, bookCount, savedBooks (this will be an array of the Book type)
-// TODO Book type: bookId (not the _id, but the book's id value returned from the Google's Book API), authors (an array of strings, as ther emay be more than one author),
-// description, title, image, link
-// TODO Autho type: token, user (references the User type)
-
-// TODO Query type: me: which returns a user type
+const { User } = require("../models");
+const { AuthenticationError } = require("apollo-server-express");
+const { signToken } = require("../utils/auth");
 
 const resolvers = {
-  Query: {
-    me: async (parent, args, context) => {
-      if (context.user) {
-        const userData = await User.findOne({ _id: context.user._id })
-          .select('-__v -password')
-          // .populate('thoughts')
-          // .populate('friends');
+  //   Query: {
+  //   me: async (parent, args, context) => {
+  //     if (context.user) {
+  //       const userData = await User.findOne({ _id: context.user._id })
+  //         .select('-__v -password')
+  //         // .populate('thoughts')
+  //         // .populate('friends');
 
-        return userData;
-      }
+  //       return userData;
+  //     }
 
-      throw new AuthenticationError('Not logged in');
-    },
-    // users: async () => {
-    //   return User.find()
-    //     .select('-__v -password')
-    //     .populate('thoughts')
-    //     .populate('friends');
-    // },
-    // user: async (parent, { username }) => {
-    //   return User.findOne({ username })
-    //     .select('-__v -password')
-    //     .populate('friends')
-    //     .populate('thoughts');
-    // },
-    // thoughts: async (parent, { username }) => {
-    //   const params = username ? { username } : {};
-    //   return Thought.find(params).sort({ createdAt: -1 });
-    // },
-    // thought: async (parent, { _id }) => {
-    //   return Thought.findOne({ _id });
-    // }
-  },
+  //     throw new AuthenticationError('Not logged in');
+  //   },
+  // },
+	Mutation: {
+		addUser: async (parent, args) => {
+			const user = await User.create(args);
+			const token = signToken(user);
+			return { token, user };
+		},
+		login: async (parent, { email, password }) => {
+			const user = await User.findOne({ email });
 
-// TODO Mutation type: 
-//  login: accepts an email and password as parameters; returns an Auth type
-//  addUser: Accepts a username, email, and password as parameters; returns an Auth type
-//  saveBook: Accepts a book author's array, description, title, bookId, image, and link as parameters; returns a User type. (look into creating whats known as an input type to handle all of those parameters!)
-//  removeBook: Accepts a book's bookId as a parameter; returns a User type
+			if (!user) {
+				throw new AuthenticationError("Incorrect credentials");
+			}
 
-  Mutation: {
-    addUser: async (parent, args) => {
-      const user = await User.create(args);
-      const token = signToken(user);
+			const correctPw = await user.isCorrectPassword(password);
 
-      return { token, user };
-    },
-    login: async (parent, { email, password }) => {
-      const user = await User.findOne({ email });
-
-      if (!user) {
-        throw new AuthenticationError('Incorrect credentials');
-      }
-
-      const correctPw = await user.isCorrectPassword(password);
-
-      if (!correctPw) {
-        throw new AuthenticationError('Incorrect credentials');
-      }
-
-      const token = signToken(user);
-      return { token, user };
-    },
+			if (!correctPw) {
+				throw new AuthenticationError("Incorrect credentials");
+			}
+			const token = signToken(user);
+			return { token, user };
+		},
     addEvent: async (parent, { eventsId }, context) => {
       if (context.user) {
         const updatedUser = await User.findOneAndUpdate(
@@ -115,37 +77,6 @@ const resolvers = {
     throw new AuthenticationError('You need to be logged in!');
     
     }
-    
-  }
-};
-
-const { User } = require("../models");
-const { AuthenticationError } = require("apollo-server-express");
-const { signToken } = require("../utils/auth");
-
-const resolvers = {
-	Mutation: {
-		addUser: async (parent, args) => {
-			const user = await User.create(args);
-			const token = signToken(user);
-			return { token, user };
-		},
-		login: async (parent, { email, password }) => {
-			const user = await User.findOne({ email });
-
-			if (!user) {
-				throw new AuthenticationError("Incorrect credentials");
-			}
-
-			const correctPw = await user.isCorrectPassword(password);
-
-			if (!correctPw) {
-				throw new AuthenticationError("Incorrect credentials");
-			}
-			const token = signToken(user);
-			return { token, user };
-		},
 	},
 };
->>>>>>> 83001c3a243dd74e85deab5e9eab436920e71714
 module.exports = resolvers;
