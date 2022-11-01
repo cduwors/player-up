f8809acc16f6a61c069952410177eeffcb6b131d
Mayfield Mayfield <mayfieldmel@gmail.com>
2022-11-01 09:20:32 -0400

    import event into list

--- client/src/components/EventList.js
+++ client/src/components/EventList.js
@@ -1,18 +1,12 @@
 import React from "react";
-
+import Event from "./Event";
 
 function EventList({events}) {    
       return (
           <ul className="event-list">
             {events.map((event) => (
               <li key={event._id} className="card">
-                <article className="text-box">
-                  <h2 className="eventName">{event.eventName}</h2>
-                  <span>{event.dateAndTime} <br />
-                  {event.location}</span>
-                  <p>Description: {event.description}</p>
-                  <p>{event.organizerNames} | Players needed: {event.numberPlayersNeeded}</p>
-                </article>
+                <Event event={event}></Event>
                 <button className="play-btn">I'm Game!</button>
               </li>
             ))}
