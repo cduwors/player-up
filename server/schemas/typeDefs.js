cc2ba4ed4f0f17764e570f0db3b5c158f08e562e
cduwors <christinaduwors@gmail.com>
2022-11-01 12:04:31 -0400

    back end errors corrected

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,43 +1,60 @@
 const { gql } = require("apollo-server-express");
 
 const typeDefs = gql`
+	type Events {
+		_id: ID
+		eventName: String
+		description: String
+		date: String
+		time: String
+		location: String
+		numberPlayersNeeded: Int
+		organizerName: String
+	}
 
-type Events {
-    _id: ID
-    eventName: String
-    description: String
-    dateAndTime: Date
-    location: String
-    numberPlayersNeeded: Int
-    organizerNames: []
-}
+	type User {
+		_id: ID
+		username: String
+		email: String
+		events: [Events]
+	}
 
-type User {
-    _id: ID
-    username: String
-    email: String
-    phone: Number
-    events: [Events]
-  }
+	type Auth {
+		token: ID
+		user: User
+	}
 
-type Auth {
-    token: ID
-    user: User
-  }
+	type Query {
+		events: [Events]
+		user: User
+	}
 
-type Query {
-    events: [Events]
-    user: User
-  }
-
-type Mutation {
-    addUser(username: String!, email: String!, password: String!): Auth
-    updateUser(username: String, email: String, password: String): User
-    login(email: String!, password: String!): Auth
-    addEvent(_id:ID!, eventName: String!, description: String!, dateAndTime: Date!, location: String!, numberPlayersNeeded: Int!, organizerNames: [String]): Events
-    updateEvent(_id:ID!, eventName: String!, description: String!, dateAndTime: Date!, location: String!, numberPlayersNeeded: Int!, organizerNames: [String]): Events
-    deleteEvent(_id:ID!): Events
-  }
-`
+	type Mutation {
+		addUser(username: String!, email: String!, password: String!): Auth
+		updateUser(username: String, email: String, password: String): User
+		login(email: String!, password: String!): Auth
+		addEvent(
+			_id: ID!
+			eventName: String!
+			description: String!
+			date: String!
+			time: String!
+			location: String!
+			numberPlayersNeeded: Int!
+			organizerName: String
+		): Events
+		updateEvent(
+			_id: ID!
+			eventName: String!
+			description: String!
+			date: String!
+			time: String!
+			location: String!
+			numberPlayersNeeded: Int!
+			organizerName: String
+		): Events
+		deleteEvent(_id: ID!): Events
+	}
+`;
 
 module.exports = typeDefs;
