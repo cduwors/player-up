b1e7e6c2ede2f258dc51c48d0518a0cf83546af2
Mayfield Mayfield <mayfieldmel@gmail.com>
2022-11-03 22:09:30 -0400

    pages rendering correctly

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,224 +1,280 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import logo from "../images/player-up-logo.png";
 import { Link } from "react-router-dom";
 import Auth from "../utils/auth";
 
-const Header = ({
-  pageSelected,
-  setPageSelected,
-  currentPage,
-  setCurrentPage,
-}) => {
- 
+const Header = () => {
+  // page selection state
+  const [pageSelected, setPageSelected] = useState(false);
+  const [homePage, setHomePage] = useState(true);
+
   // update document.title to match category selection
   useEffect(() => {
-    document.title = currentPage;
-  }, [currentPage]);
+    document.title = "PlayerUP"});
+
+  const displayNav = () => {
+      return (
+      <div className="header">
+        <a href="/" onClick={() => setHomePage("Home")}>
+          <div className="logo-header">
+            <img className="logo" src={logo} alt="player-up-logo"></img>
+          </div>
+        </a>
+        <nav>
+          <ul className="nav-header">
+            <li
+              className={`nav-link ${pageSelected && "navActive"}`}
+              onClick={() => {
+                setHomePage(false);
+                setPageSelected(true);
+              }}
+            >
+              <Link style={{ textDecoration: "none" }} to={"/profile"}>
+                Profile
+              </Link>
+            </li>
+            <li
+              className={`nav-link ${pageSelected && "navActive"}`}
+              onClick={() => {
+                setHomePage(false);
+                setPageSelected(true);
+              }}
+            >
+              <Link style={{ textDecoration: "none" }} to={"/events"}>
+                Events
+              </Link>
+            </li>
+            <li
+              className={`nav-link ${pageSelected && "navActive"}`}
+              onClick={() => {
+                setHomePage(false);
+                setPageSelected(true);
+              }}
+            >
+              <Link
+                style={{ textDecoration: "none" }}
+                to={"/"}
+                onClick={() => Auth.logout()}
+              >
+                Logout
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    )}
+
+const displayNavHome = () => {
+return (
+<div className="header">
+              <a className="hide" href="/" onClick={() => setHomePage("Home")}>
+                <div className="logo-header">
+                  <img className="logo" src={logo} alt="player-up-logo"></img>
+                </div>
+              </a>
+              <div className="nav-home">
+                <nav>
+                  <ul className="nav-header">
+                    <li
+                      className={`nav-link-home ${pageSelected && "navActive"}`}
+                      onClick={() => {
+                        setHomePage(false);
+                        setPageSelected(true);
+                      }}
+                    >
+                      <Link
+                        style={{
+                          textDecoration: "none",
+                          color: "#ffff",
+                          marginTop: "15px",
+                        }}
+                        to={"/profile"}
+                        className="link-text"
+                      >
+                        Profile
+                      </Link>
+                    </li>
+                    <li
+                      className={`nav-link-home ${pageSelected && "navActive"}`}
+                      onClick={() => {
+                        setHomePage(false);
+                        setPageSelected(true);
+                      }}
+                    >
+                      <Link
+                        style={{
+                          textDecoration: "none",
+                          color: "#ffff",
+                          marginTop: "15px",
+                        }}
+                        to={"/events"}
+                        className="link-text"
+                      >
+                        Events
+                      </Link>
+                    </li>
+                    <li
+                      className={`nav-link-home ${pageSelected && "navActive"}`}
+                      onClick={() => {
+                        setHomePage(false);
+                        setPageSelected(true);
+                      }}
+                    >
+                      <Link
+                        style={{
+                          textDecoration: "none",
+                          color: "#ffff",
+                          marginTop: "15px",
+                        }}
+                        to={"/"}
+                        onClick={() => Auth.logout()}
+                        className="link-text"
+                      >
+                        Logout
+                      </Link>
+                    </li>
+                  </ul>
+                </nav>
+              </div>
+            </div>
+          )}
 
   return (
-    <> {Auth.loggedIn() ?
-    (<header>
-      {currentPage !== "Home" ? (
-        <div className="header">
-          <a href="/" onClick={() => setCurrentPage("Home")}>
-            <div className="logo-header">
-              <img className="logo" src={logo} alt="player-up-logo"></img>
-            </div>
-          </a>
-          <nav>
-            <ul className="nav-header">
-              <li
-                className={`nav-link ${
-                  currentPage === "Profile" && pageSelected && "navActive"
-                }`}
-                onClick={() => {
-                  setCurrentPage("Profile");
-                  setPageSelected(true);
-                }}
-              >
-                <Link style={{textDecoration: 'none'}} to={"/profile"}>Profile</Link>
-              </li>
-              <li
-                className={`nav-link ${
-                  currentPage === "Events" && pageSelected && "navActive"
-                }`}
-                onClick={() => {
-                  setCurrentPage("Events");
-                  setPageSelected(true);
-                }}
-              >
-                <Link style={{textDecoration: 'none'}} to={"/events"}>Events</Link>
-              </li>
-              <li
-                className={`nav-link ${
-                  currentPage === "Logout" && pageSelected && "navActive"
-                }`}
-                onClick={() => {
-                  setCurrentPage("Logout");
-                  setPageSelected(true);
-                }}
-              >
-                <Link style={{textDecoration: 'none'}} to={"/"} onClick={() => Auth.logout()}>Logout</Link>
-              </li>
-            </ul>
-          </nav>
-        </div>
+    <>
+      {Auth.loggedIn() ? (
+        <header>
+          {homePage ? displayNavHome() : displayNav()}
+        </header>
       ) : (
-        <div className="header">
-          <a className="hide" href="/" onClick={() => setCurrentPage("Home")}>
-            <div className="logo-header">
-              <img className="logo" src={logo} alt="player-up-logo"></img>
-            </div>
-          </a>
-          <div className="nav-home">
-            <nav>
-              <ul className="nav-header">
-                <li
-                  className={`nav-link-home ${
-                    currentPage === "Profile" &&
-                    pageSelected &&
-                    "navActive"
-                  }`}
-                  onClick={() => {
-                    setCurrentPage("Profile");
-                    setPageSelected(true);
-                  }}
-                >
-                  <Link style={{textDecoration: 'none', color: '#ffff', marginTop: '15px'}} to={"/profile"} className="link-text">Profile</Link>
-                </li>
-                <li
-                  className={`nav-link-home ${
-                    currentPage === "Events" &&
-                    pageSelected &&
-                    "navActive"
-                  }`}
-                  onClick={() => {
-                    setCurrentPage("Events");
-                    setPageSelected(true);
-                  }}
-                >
-                  <Link style={{textDecoration: 'none', color: '#ffff', marginTop: '15px'}} to={"/events"} className="link-text">Events</Link>
-                </li>
-                <li
-                  className={`nav-link-home ${
-                    currentPage === "Logout" &&
-                    pageSelected &&
-                    "navActive"
-                  }`}
-                  onClick={() => {
-                    setCurrentPage("Logout");
-                    setPageSelected(true);
-                  }}
-                >
-                  <Link style={{textDecoration: 'none', color: '#ffff', marginTop: '15px'}} to={"/"} onClick={() => Auth.logout()} className="link-text">Logout</Link>
-                </li>
+        <header>
+          {!homePage ? (
+            <div className="header">
+              <a href="/" onClick={() => setHomePage("Home")}>
+                <div className="logo-header">
+                  <img className="logo" src={logo} alt="player-up-logo"></img>
+                </div>
+              </a>
+              <nav>
+                <ul className="nav-header">
+                  <li
+                    className={`nav-link ${pageSelected && "navActive"}`}
+                    onClick={() => {
+                      setHomePage(false);
+                      setPageSelected(true);
+                    }}
+                  >
+                    <Link style={{ textDecoration: "none" }} to={"/events"}>
+                      Events
+                    </Link>
+                  </li>
+                  <li
+                    className={`nav-link ${pageSelected && "navActive"}`}
+                    onClick={() => {
+                      setHomePage(false);
+                      setPageSelected(true);
+                    }}
+                  >
+                    <Link style={{ textDecoration: "none" }} to={"/login"}>
+                      Login
+                    </Link>
+                  </li>
+                  <li
+                    className={`nav-link ${
+                      pageSelected && "navActive"
+                    }`}
+                    onClick={() => {
+                      setHomePage(false);
+                      setPageSelected(true);
+                    }}
+                  >
+                    <Link style={{ textDecoration: "none" }} to={"/signup"}>
+                      Signup
+                    </Link>
+                  </li>
                 </ul>
-            </nav>
-          </div>
-        </div>
-      )}
-    </header>) : (<header>
-      {currentPage !== "Home" ? (
-        <div className="header">
-          <a href="/" onClick={() => setCurrentPage("Home")}>
-            <div className="logo-header">
-              <img className="logo" src={logo} alt="player-up-logo"></img>
+              </nav>
             </div>
-          </a>
-          <nav>
-            <ul className="nav-header">
-              <li
-                className={`nav-link ${
-                  currentPage === "Events" && pageSelected && "navActive"
-                }`}
-                onClick={() => {
-                  setCurrentPage("Events");
-                  setPageSelected(true);
-                }}
-              >
-                <Link style={{textDecoration: 'none'}} to={"/events"}>Events</Link>
-              </li>
-              <li
-                className={`nav-link ${
-                  currentPage === "Login" && pageSelected && "navActive"
-                }`}
-                onClick={() => {
-                  setCurrentPage("Login");
-                  setPageSelected(true);
-                }}
-              >
-                <Link style={{textDecoration: 'none'}} to={"/login"}>Login</Link>
-              </li>
-              <li
-                className={`nav-link ${
-                  currentPage === "Signup" && pageSelected && "navActive"
-                }`}
-                onClick={() => {
-                  setCurrentPage("Signup");
-                  setPageSelected(true);
-                }}
-              >
-                <Link style={{textDecoration: 'none'}} to={"/signup"}>Signup</Link>
-              </li>
-            </ul>
-          </nav>
-        </div>
-      ) : (
-        <div className="header">
-          <a className="hide" href="/" onClick={() => setCurrentPage("Home")}>
-            <div className="logo-header">
-              <img className="logo" src={logo} alt="player-up-logo"></img>
+          ) : (
+            <div className="header">
+              <a className="hide" href="/" onClick={() => setHomePage("Home")}>
+                <div className="logo-header">
+                  <img className="logo" src={logo} alt="player-up-logo"></img>
+                </div>
+              </a>
+              <div className="nav-home">
+                <nav>
+                  <ul className="nav-header">
+                    <li
+                      className={`nav-link-home ${
+                        pageSelected && "navActive"
+                      }`}
+                      onClick={() => {
+                        setHomePage(false);
+                        setPageSelected(true);
+                      }}
+                    >
+                      <Link
+                        style={{
+                          textDecoration: "none",
+                          color: "#ffff",
+                          marginTop: "15px",
+                        }}
+                        to={"/events"}
+                        className="link-text"
+                      >
+                        Events
+                      </Link>
+                    </li>
+                    <li
+                      className={`nav-link-home ${
+                        pageSelected && "navActive"
+                      }`}
+                      onClick={() => {
+                        setHomePage(false);
+                        setPageSelected(true);
+                      }}
+                    >
+                      <Link
+                        style={{
+                          textDecoration: "none",
+                          color: "#ffff",
+                          marginTop: "15px",
+                        }}
+                        to={"/login"}
+                        className="link-text"
+                      >
+                        Login
+                      </Link>
+                    </li>
+                    <li
+                      className={`nav-link-home ${
+                        pageSelected && "navActive"
+                      }`}
+                      onClick={() => {
+                        setHomePage(false);
+                        setPageSelected(true);
+                      }}
+                    >
+                      <Link
+                        style={{
+                          textDecoration: "none",
+                          color: "#ffff",
+                          marginTop: "15px",
+                        }}
+                        to={"/signup"}
+                        className="link-text"
+                      >
+                        Signup
+                      </Link>
+                    </li>
+                  </ul>
+                </nav>
+              </div>
             </div>
-          </a>
-          <div className="nav-home">
-            <nav>
-              <ul className="nav-header">
-                <li
-                  className={`nav-link-home ${
-                    currentPage === "Events" &&
-                    pageSelected &&
-                    "navActive"
-                  }`}
-                  onClick={() => {
-                    setCurrentPage("Events");
-                    setPageSelected(true);
-                  }}
-                >
-                  <Link style={{textDecoration: 'none', color: '#ffff', marginTop: '15px'}} to={"/events"} className="link-text">Events</Link>
-                </li>
-                <li
-                  className={`nav-link-home ${
-                    currentPage === "Login" &&
-                    pageSelected &&
-                    "navActive"
-                  }`}
-                  onClick={() => {
-                    setCurrentPage("Login");
-                    setPageSelected(true);
-                  }}
-                >
-                  <Link style={{textDecoration: 'none', color: '#ffff', marginTop: '15px'}} to={"/login"} className="link-text">Login</Link>
-                </li>
-                <li
-                  className={`nav-link-home ${
-                    currentPage === "Signup" &&
-                    pageSelected &&
-                    "navActive"
-                  }`}
-                  onClick={() => {
-                    setCurrentPage("Signup");
-                    setPageSelected(true);
-                  }}
-                >
-                  <Link style={{textDecoration: 'none', color: '#ffff', marginTop: '15px'}} to={"/signup"} className="link-text">Signup</Link>
-                </li>
-                </ul>
-            </nav>
-          </div>
-        </div>
+          )}
+        </header>
       )}
-    </header>)
-}</>
+    </>
   );
 };
 
